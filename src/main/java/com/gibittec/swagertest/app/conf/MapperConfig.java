d4a1c2bac73b420c97f94706abda32c2322cd510
Wellington <wellington.adames.p@gmail.com>
2021-03-27 17:06:56 -0400

    Refactoring Package name.

--- src/main/java/com/gibittec/swagertest/app/conf/MapperConfig.java
+++ src/main/java/com/gibittec/swagertest/app/conf/MapperConfig.java
@@ -1,4 +1,4 @@
-package com.gibittec.swagertest.app.util;
+package com.gibittec.swagertest.app.conf;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/gibittec/swagertest/app/conf/SwaggerConfiguration.java
+++ src/main/java/com/gibittec/swagertest/app/conf/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.gibittec.swagertest.app.util;
+package com.gibittec.swagertest.app.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
